341659c06a33e57d5b90b9d03ed04c0d7dc8b0ca
SaiSeng18 <saisengmongli14@gmail.com>
2024-06-14 23:00:58 +0630

    chore: tab management

--- utils/store/index.ts
+++ utils/store/index.ts
@@ -1,42 +1,59 @@
-import create from "zustand";
+import { create } from "zustand";
 import {
 	getTasks,
 	getTasksByCompletion,
 	addTask as addTaskToDB,
+	updateTaskCompletion,
 } from "@/utils/db";
-import { useSQLiteContext } from "expo-sqlite/build";
+import { SQLiteDatabase } from "expo-sqlite/build";
 import { Task } from "@/utils/db/types";
 
 interface TasksStore {
 	tasks: Task[];
-	fetchAll: () => Promise<void>;
-	fetchCompleted: () => Promise<void>;
-	fetchInProgress: () => Promise<void>;
+	fetchAll: (db: SQLiteDatabase) => Promise<void>;
+	fetchCompleted: (db: SQLiteDatabase) => Promise<void>;
+	fetchInProgress: (db: SQLiteDatabase) => Promise<void>;
 	addTask: (
+		db: SQLiteDatabase,
 		title: string,
 		description: string,
 		completed: boolean,
 		tags: string[]
 	) => Promise<void>;
+	handleCompletion: (
+		db: SQLiteDatabase,
+		id: number,
+		completed: boolean
+	) => Promise<void>;
+	activeTab: "all" | "completed" | "in progress";
+	setActiveTab: (tab: "all" | "completed" | "in progress") => void;
 }
-const db = useSQLiteContext();
 const useTasksStore = create<TasksStore>((set, get) => ({
 	tasks: [],
-	fetchAll: async () => {
+	activeTab: "all",
+	setActiveTab: (tab) => set({ activeTab: tab }),
+	fetchAll: async (db) => {
 		const data = await getTasks(db);
 		set({ tasks: data });
+		set({ activeTab: "all" });
 	},
-	fetchCompleted: async () => {
+	fetchCompleted: async (db) => {
 		const data = await getTasksByCompletion(db, true);
 		set({ tasks: data });
+		set({ activeTab: "completed" });
 	},
-	fetchInProgress: async () => {
+	fetchInProgress: async (db) => {
 		const data = await getTasksByCompletion(db, false);
 		set({ tasks: data });
+		set({ activeTab: "in progress" });
 	},
-	addTask: async (title, description, completed, tags) => {
+	addTask: async (db, title, description, completed, tags) => {
 		await addTaskToDB(db, title, description, completed, tags);
-		get().fetchAll();
+		get().fetchAll(db);
+	},
+	handleCompletion: async (db, id, completed) => {
+		await updateTaskCompletion(db, id, completed);
+		get().fetchCompleted(db);
 	},
 }));
 
